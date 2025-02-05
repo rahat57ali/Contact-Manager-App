79270af900e3bfcd2dee2aca538740d95238013c
Rahat <rahatalisoomro10@gmail.com>
2025-02-06 00:34:06 +0500

    Created other routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/').get((req, res) => {
-    res.status(200).json({message: "Get all contacts"});
+    res.status(200).json({message: 'Get all contacts'});
 });
 
+router.route('/').post((req, res) => {
+    res.status(200).json({message: 'Create Contacts'});
+});
+
+router.route('/:id').get((req, res) => {
+    res.status(200).json({message: `Get contacts for ${req.params.id}`});
+});
+
+router.route('/:id').put((req, res) => {
+    res.status(200).json({message: `Update contacts for ${req.params.id}`});
+});
+
+router.route('/:id').delete((req, res) => {
+    res.status(200).json({message: `Delete contacts for ${req.params.id}`});
+});
 module.exports = router;
