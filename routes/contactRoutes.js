520d63ba832e22d4fa07c08e97f753e99ab24043
Rahat <rahatalisoomro10@gmail.com>
2025-02-07 23:40:38 +0500

    Multiple HTTP Methods per Route

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -17,4 +17,7 @@ router.route('/:id').get(getContact);
 router.route('/:id').put(updateContact);
 
 router.route('/:id').delete(deleteContact);
+
+router.route('/:id').delete(deleteContact);
+
 module.exports = router;
