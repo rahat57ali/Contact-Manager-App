4787dcb3b250f85bf1c1818cbf816f185436e356
Rahat <rahatalisoomro10@gmail.com>
2025-02-15 20:38:45 +0500

    Express async handler.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,15 @@
+const asyncHandler = require('express-async-handler');
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get all contacts'});
-};
+});
 
 //@desc Create new contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is: ",req.body);
     const {name, email, phone} = req.body;
     if(!name || !email || !phone){
@@ -16,27 +17,27 @@ const createContact = (req, res) => {
         throw new Error("All fields are mandatory.")
     }
     res.status(201).json({message: 'Create Contacts'});
-};
+});
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Get contacts for ${req.params.id}`});
-};
+});
 
 //@desc Update contact
 //@route POST /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Update contacts for ${req.params.id}`});
-};
+});
 
 //@desc Delete contact
 //@route PUT /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Delete contacts for ${req.params.id}`});
-};
+});
 
 module.exports = { getContact, createContact, getContacts, updateContact, deleteContact };
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,28 @@
+const {constants} = require("../constants");
 const { stack } = require("../routes/contactRoutes");
+
 
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
-    res.json({message: err.message, stackTrace: err.stack});
+    switch (statusCode) {
+        case NOT_FOUND:
+            res.json({title: "Not found",message: err.message, stackTrace: err.stack});
+            break;
+        case UNAUTHORIZED:
+            res.json({title: "Unauthorized.",message: err.message, stackTrace: err.stack});
+            break;
+        case FORBIDDEN:
+            res.json({title: "Forbidden.",message: err.message, stackTrace: err.stack});
+            break;
+        case SERVER_ERROR:
+            res.json({title: "Server error.",message: err.message, stackTrace: err.stack});
+            break;
+        default:
+            console.log("No error, all good.");
+            break;
+    }
+    
+    
 };
 
 module.exports = errorHandler
