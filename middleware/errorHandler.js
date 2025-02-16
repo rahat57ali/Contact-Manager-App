423058a740140f1b2ca5072f24d106818175dc0d
Rahat <rahatalisoomro10@gmail.com>
2025-02-17 01:39:30 +0500

    Setting up Mongodb

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,20 +1,20 @@
-const {constants} = require("../constants");
+const {constants} = require("../constants"); 
 const { stack } = require("../routes/contactRoutes");
 
 
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch (statusCode) {
-        case NOT_FOUND:
+        case constants.NOT_FOUND:
             res.json({title: "Not found",message: err.message, stackTrace: err.stack});
             break;
-        case UNAUTHORIZED:
+        case constants.UNAUTHORIZED:
             res.json({title: "Unauthorized.",message: err.message, stackTrace: err.stack});
             break;
-        case FORBIDDEN:
+        case constants.FORBIDDEN:
             res.json({title: "Forbidden.",message: err.message, stackTrace: err.stack});
             break;
-        case SERVER_ERROR:
+        case constants.SERVER_ERROR:
             res.json({title: "Server error.",message: err.message, stackTrace: err.stack});
             break;
         default:
